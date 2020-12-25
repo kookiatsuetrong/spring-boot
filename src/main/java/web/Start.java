9996344c8ab0a4a0740780799e654d6ccce7b097
Kookiat Suetrong <beer@Kookiats-MacBook-Air.local>
2020-12-25 14:33:55 +0700

    Keep Walking

---
+++ src/main/java/web/Start.java
@@ -0,0 +1,10 @@
+package web;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Start {
+	public static void main(String[] z) {
+		SpringApplication.run(Start.class, z);
+	}
+}
